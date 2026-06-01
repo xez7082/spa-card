18343ef40ca631ae56672fe70bb9ae16b9865a4c
xez7082 <107535456+xez7082@users.noreply.github.com>
2026-06-01 21:42:57 +0200

    Update spa-styles.js
    Signed-off-by: xez7082 <107535456+xez7082@users.noreply.github.com>

--- spa-styles.js
+++ spa-styles.js
@@ -1,6 +1,6 @@
 import { css } from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
 
-// Le mot "export" est obligatoire pour rendre le style accessible
+// Le mot "export" ici est indispensable
 export const sharedStyles = css`
   :host {
     display: block;
