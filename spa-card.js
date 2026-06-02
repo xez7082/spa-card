b1d3ad58ce06f5e99fdbec4c6b41fb5287673c08
xez7082 <107535456+xez7082@users.noreply.github.com>
2026-06-02 12:04:02 +0200

    Update spa-card.js
    Signed-off-by: xez7082 <107535456+xez7082@users.noreply.github.com>

--- spa-card.js
+++ spa-card.js
@@ -2,43 +2,43 @@ browser_mod.js?2.13.5:608 BROWSER_MOD 2.13.5 IS INSTALLED    BrowserID: browser_
 spa:1 Unchecked runtime.lastError: Could not establish connection. Receiving end does not exist.
 button-card.js?hacstag=146194325701:50  BUTTON-CARD  v7.0.1 
 layout-card.js?hacstag=156434866247:1 LAYOUT-CARD 2.4.7 IS INSTALLED
+fluid-level-background-card.js?hacstag=4319015130186:282   fluid-level-background-card   Version 0.1.8.6    
 card-mod.js?hacstag=190927524421:1 CARD-MOD 4.2.1 IS INSTALLED
 card-mod.js?hacstag=190927524421:1 You may not be getting optimal performance out of card-mod.
 See https://github.com/thomasloven/lovelace-card-mod#performance-improvements
-fluid-level-background-card.js?hacstag=4319015130186:282   fluid-level-background-card   Version 0.1.8.6    
 navbar-card.js?hacstag=871106499161:5260  navbar-cardv1.6.1 
 slider-button-card.js?hacstag=4987940331130:1043   SLIDER-BUTTON-CARD  v1.13.0 
 timer-bar-card.js?hacstag=3769045171311:590  TIMER-BAR-CARD  Version 1.31.0 
 entity-progress-card.js?hacstag=908868522152:1475 ✨ENTITY-PROGRESS-CARD 1.5.2 IS INSTALLED.
 apexcharts-card.js?hacstag=331701152223:950  APEXCHARTS-CARD  v2.2.3 
+mushroom.js?hacstag=444350375511:61 🍄 Mushroom 🍄 - 5.1.1
 ring-tile-card.js?hacstag=991075417106:747  ring-tile-card  v1.0.6   #8c8d43b-251229-0537-prod 
 auto-entities.js?hacstag=1677445841161:1 AUTO-ENTITIES 1.16.1 IS INSTALLED
-mushroom.js?hacstag=444350375511:61 🍄 Mushroom 🍄 - 5.1.1
 area-card-plus.js?hacstag=911369599123:5853  AREA-CARD  v1.2.1 
-badge-card.js?hacstag=2814536085672:6 BADGE-CARD 1.0.1 IS INSTALLED 
 template-entity-row.js?hacstag=231674882141:1 TEMPLATE-ENTITY-ROW 1.4.1 IS INSTALLED 
+badge-card.js?hacstag=2814536085672:6 BADGE-CARD 1.0.1 IS INSTALLED 
 multiple-entity-row.js?hacstag=178921037451:1  MULTIPLE-ENTITY-ROW  4.5.1 
-weather-forecast-card.js?hacstag=1110718167100:557 WEATHER-FORECAST-CARD 1.0.0
-meteoalarm-card.js:818  MeteoalarmCard  2.7.2 
-hui-element.js?hacstag=249942054180547:1 HUI-ELEMENT 1.1.2 IS INSTALLED 
-clock_pv_forecast_card.js?hacstag=998247638203:8 📦 clock-pv-forecast-card v2.0.1 loaded (Smooth Operator)
 clock-weather-card.js?hacstag=522634019293:180   CLOCK-WEATHER-CARD  Version: 2.9.3
+clock_pv_forecast_card.js?hacstag=998247638203:8 📦 clock-pv-forecast-card v2.0.1 loaded (Smooth Operator)
+weather-forecast-card.js?hacstag=1110718167100:557 WEATHER-FORECAST-CARD 1.0.0
+hui-element.js?hacstag=249942054180547:1 HUI-ELEMENT 1.1.2 IS INSTALLED 
+meteoalarm-card.js:818  MeteoalarmCard  2.7.2 
 directive.js:6   BUTTON-TEXT-CARD   v0.6.6    
 custom-icons.js?hacstag=491465538037:309   Custom-icons    Version 0.3.7 20221004
 meteocss-card.js?hacstag=1129986225300:1  🙂 MeteoCSS Card  v3.0.0
 decluttering-card.js:186  DECLUTTERING-CARD    Version 1.0.0   
 flex-table-card.js?hacstag=15629205814:1561  FLEX-TABLE-CARD  Version 1.4.0 
 animated-battery-card.js:317  ANIMATED-BATTERY-CARD  v1.2.0-DEBUG 
-person-tracker-card.js?hacstag=11019109771413:45 Person Tracker Card v1.4.13 Multilanguage loading...
-person-tracker-card.js?hacstag=11019109771413:6277  PERSON-TRACKER-CARD  v1.4.13 !
 stack-in-card.js?hacstag=248954055020:200  STACK-IN-CARD    Version 0.2.0   
 kiosk-mode.js?hacstag=4973191281310:1 ≡ kiosk-mode             ⋮
 version 13.0.0           ⋮
+person-tracker-card.js?hacstag=11019109771413:45 Person Tracker Card v1.4.13 Multilanguage loading...
+person-tracker-card.js?hacstag=11019109771413:6277  PERSON-TRACKER-CARD  v1.4.13 !
 power-flow-card-plus.js?hacstag=618081815037:1 ⚡ Power Flow Card Plus v0.3.7 is installed
-persons-tracker-card.js?v=3.0:944  PERSONS-TRACKER-CARD  v3.0.0 
-etage-floor-card.js?v=2.0.1:604  ÉTAGE-CARD  v5.0 
 plant-card.js?v=1.2.0:612  PLANT-DASHBOARD-CARD  v2.0.0 
+etage-floor-card.js?v=2.0.1:604  ÉTAGE-CARD  v5.0 
+:8123/hacsfiles/spa-…tag=1150857844429:1 Uncaught SyntaxError: Unexpected number
+persons-tracker-card.js?v=3.0:944  PERSONS-TRACKER-CARD  v3.0.0 
 rdc-floor-card.js?v=2.0.1:1069  RDC-FLOOR-CARD  v8.0 
 streamline-card.js?hacstag=839495072022:6418  Streamline Card  0.2.2
-:8123/hacsfiles/spa-…tag=1150857844428:1 Uncaught SyntaxError: Unexpected identifier 'le'
 card-294f2ffb.js:557  📷 Advanced Camera Card  7.27.4 
