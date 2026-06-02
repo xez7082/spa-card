5a9834eea3ee31f7ff4693c6d5a996009262b613
xez7082 <107535456+xez7082@users.noreply.github.com>
2026-06-02 08:15:46 +0200

    Delete spa-styles.js
    Signed-off-by: xez7082 <107535456+xez7082@users.noreply.github.com>

--- spa-styles.js
+++
@@ -1,8 +0,0 @@
-import { css } from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
-
-export const sharedStyles = css`
-  :host {
-    display: block;
-    box-sizing: border-box;
-  }
-`;
